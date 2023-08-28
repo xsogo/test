f5719eb3e3e761a1fc1aa9eced71305aed815797
xsogo <xsogo@hotmail.com>
2023-08-28 12:19:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,58 @@
-var slideIndex = 0; // Current slide index
-var playPause = true; // Play/Pause flag
-var timerVar; // Timer variable for autoplay
-var intervalDuration = 5000; // Interval between slide changes (milliseconds)
+let currentFolder = 'pressure';
+let currentImageIndex = 0;
+let isPlaying = false;
+const imageDisplay = document.getElementById('imageDisplay');
+const slider = document.getElementById('slider');
+const playPauseButton = document.getElementById('playPause');
 
-// Add your image filenames to the arrays
-var pressureImages = [
-    "Pressure_xz-0000.png",
-    "Pressure_xz-0001.png",
-    "Pressure_xz-0002.png",
-    "Pressure_xz-0003.png",
-    "Pressure_xz-0004.png"
-    // ... Add more images
-];
-
-var densityImages = [
-    "Density_xz-0000.png",
-    "Density_xz-0001.png",
-    "Density_xz-0002.png",
-    "Density_xz-0003.png",
-    "Density_xz-0004.png"
-    // ... Add more images
-];
-
-// Initialize the slideshow
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    clearTimeout(timerVar);
-    showSlides((slideIndex += n));
+function updateImage() {
+    imageDisplay.src = `${currentFolder}/${currentImageIndex}.png`;
 }
 
-// Play/Pause control
-function playPauseHandler() {
-    playPause = !playPause;
-    var playPauseButtonIcon = document.getElementById("play-pause-icon");
-    
-    if (playPause) {
-        playPauseButtonIcon.className = "fa fa-pause";
-        showSlides(slideIndex);
+function playPause() {
+    isPlaying = !isPlaying;
+    if (isPlaying) {
+        playPauseButton.innerText = 'Pause';
+        advanceImage();
     } else {
-        playPauseButtonIcon.className = "fa fa-play";
-        clearTimeout(timerVar);
+        playPauseButton.innerText = 'Play';
+        clearInterval(interval);
     }
 }
 
-// Play the pressure images
-function playPressure() {
-    clearTimeout(timerVar);
-    playImages(pressureImages);
+let interval;
+
+function advanceImage() {
+    interval = setInterval(() => {
+        currentImageIndex++;
+        if (currentImageIndex > 99) {
+            currentImageIndex = 0;
+        }
+        updateImage();
+        slider.value = currentImageIndex;
+    }, 1000); // 每秒播放一张，您可以调整这个值
 }
 
-// Play the density images
-function playDensity() {
-    clearTimeout(timerVar);
-    playImages(densityImages);
-}
+slider.addEventListener('input', (e) => {
+    currentImageIndex = parseInt(e.target.value);
+    updateImage();
+});
 
-// Function to play a set of images
-function playImages(images) {
-    slideIndex = 0;
-    showSlides(slideIndex, images);
-    timerVar = setInterval(function () {
-        plusSlides(1);
-    }, intervalDuration);
-}
+playPauseButton.addEventListener('click', playPause);
 
-// Function to show slides
-function showSlides(n, imageArray) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var slideNumber = document.getElementById("slide-number");
-    
-    if (!imageArray) {
-        imageArray = pressureImages; // Default to pressure images
-    }
-    
-    if (n >= imageArray.length) {
-        slideIndex = 0;
-    }
-    if (n < 0) {
-        slideIndex = imageArray.length - 1;
-    }
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slideNumber.innerHTML = slideIndex + 1 + " / " + imageArray.length;
-    slides[slideIndex].style.display = "block";
-    
-    if (playPause) {
-        timerVar = setTimeout(function () {
-            plusSlides(1);
-        }, intervalDuration);
-    }
-}
+document.getElementById('pressure').addEventListener('click', () => {
+    currentFolder = 'pressure';
+    currentImageIndex = 0;
+    updateImage();
+    slider.value = 0;
+});
+
+document.getElementById('density').addEventListener('click', () => {
+    currentFolder = 'density';
+    currentImageIndex = 0;
+    updateImage();
+    slider.value = 0;
+});
+
+// 初始化
+updateImage();
